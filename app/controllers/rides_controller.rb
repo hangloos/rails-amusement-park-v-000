bb0289c248a3463d9b0c5df9e85602f64ba2893d
hangloos <ericloos00@gmail.com>
2016-08-08 15:24:29 -0400

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -2,7 +2,7 @@ class RidesController < ApplicationController
   
 
   def create
-    @ride = Ride.new(:attraction_id => params[:attraction_id], :user_id => params[:user_id])
+    @ride = Ride.create(:attraction_id => params[:attraction_id], :user_id => params[:user_id])
     @ride_information = @ride.take_ride
     redirect_to user_path(@ride.user, :ride_info => @ride_information)
   end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,38 +3,33 @@ class Ride < ActiveRecord::Base
   belongs_to :user
 
   
-
   def enough_tickets?
-    if self.user.tickets < attraction.tickets
-      "Sorry. You do not have enough tickets the #{attraction.name}."
-    end
+    user.tickets >= attraction.tickets
   end
 
   def tall_enough?
-    if self.user.height < self.attraction.min_height
-      "Sorry. You are not tall enough to ride the #{attraction.name}."
-    end
+    user.height >= attraction.min_height
   end
 
   def take_ride
-    if enough_tickets? && tall_enough?
-      "Sorry. You do not have enough tickets the #{attraction.name}. You are not tall enough to ride the Roller Coaster."
-    elsif enough_tickets?
-      enough_tickets?
-    elsif tall_enough?
-      tall_enough?
+    if !enough_tickets? && !tall_enough?
+      return "Sorry. You do not have enough tickets the #{attraction.name}. You are not tall enough to ride the Roller Coaster."
+    elsif !enough_tickets?
+      return "Sorry. You do not have enough tickets the #{attraction.name}."
+    elsif !tall_enough?
+      return "Sorry. You are not tall enough to ride the #{attraction.name}."
     else
       user = self.user
 
-      new_happiness = user.happiness + self.attraction.happiness_rating
-      new_nausea = user.nausea + self.attraction.nausea_rating
-      new_tickets =  user.tickets - self.attraction.tickets
+      new_happiness = user.happiness + attraction.happiness_rating
+      new_nausea = user.nausea + attraction.nausea_rating
+      new_tickets =  user.tickets - attraction.tickets
 
       user.update!(happiness: new_happiness)
       user.update!(nausea: new_nausea)
       user.update!(tickets: new_tickets)
       user.save
-      "Thanks for riding the #{self.attraction.name}!"
+      return "Thanks for riding the #{attraction.name}!"
     end
   end
 end
